173e94c7afd3eff85d01c1a8ea67d41dc6bf5244
Hai Nguyen <hai427@uw.edu>
2016-10-24 19:50:11 -0700

    basic operations work

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstInput = Double()
+    var secondInput = Double()
+    var operationInput = String()
+    var operationSelected = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,50 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
-
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if operationSelected {
+            resultLabel.text = ""
+            operationSelected = false
+        }
+        resultLabel.text = resultLabel.text! + sender.titleLabel!.text!
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        if resultLabel.text! != "" {
+            firstInput = Double(resultLabel.text!)!
+            operationInput = sender.titleLabel!.text!
+            resultLabel.text = sender.titleLabel!.text!
+            operationSelected = true
+        }
+    }
+    
+    @IBAction func equalsPressed(_ sender: UIButton) {
+        if resultLabel.text! != "" {
+            secondInput = Double(resultLabel.text!)!
+            switch operationInput {
+            case "+":
+                resultLabel.text = String(firstInput + secondInput)
+            case "-":
+                resultLabel.text = String(firstInput - secondInput)
+            case "*":
+                resultLabel.text = String(firstInput * secondInput)
+            case "/":
+                resultLabel.text = String(firstInput / secondInput)
+            default:
+                resultLabel.text = ""
+            }
+            firstInput = 0.0
+            secondInput = 0.0
+            operationInput = ""
+            operationSelected = false
+            resultLabel.text = ""
+        }
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        resultLabel.text = nil
+    }
 }
-
