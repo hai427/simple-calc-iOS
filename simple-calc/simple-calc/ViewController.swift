a0fb30ac949008285b30dd60c9ebc0922815fba6
Hai Nguyen <hai427@gmail.com>
2016-10-25 17:04:39 -0700

    added color, added other operands

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var firstInput = Double()
-    var secondInput = Double()
+//    var firstInput = Double()
+//    var secondInput = Double()
+    var input = [Double]()
     var operationInput = String()
-    var operationSelected = false
+    var operationEntered = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,46 +29,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
 
     @IBAction func numberPressed(_ sender: UIButton) {
-        if operationSelected {
+        if resultLabel.text != "" {
             resultLabel.text = ""
-            operationSelected = false
+        }
+        if operationEntered {
+            resultLabel.text = ""
+            operationEntered = false
         }
         resultLabel.text = resultLabel.text! + sender.titleLabel!.text!
     }
     
     @IBAction func operationPressed(_ sender: UIButton) {
         if resultLabel.text! != "" {
-            firstInput = Double(resultLabel.text!)!
+            input.append(Double(resultLabel.text!)!)
             operationInput = sender.titleLabel!.text!
             resultLabel.text = sender.titleLabel!.text!
-            operationSelected = true
+            operationEntered = true
         }
     }
     
     @IBAction func equalsPressed(_ sender: UIButton) {
-        if resultLabel.text! != "" {
-            secondInput = Double(resultLabel.text!)!
+        if Double(resultLabel.text!) != nil {
+            input.append(Double(resultLabel.text!)!)
+        }
+        
+        if operationInput != "" && input.count >= 2 {
             switch operationInput {
             case "+":
-                resultLabel.text = String(firstInput + secondInput)
+                resultLabel.text = String(input[0] + input[1])
             case "-":
-                resultLabel.text = String(firstInput - secondInput)
+                resultLabel.text = String(input[0] - input[1])
             case "*":
-                resultLabel.text = String(firstInput * secondInput)
+                resultLabel.text = String(input[0] * input[1])
             case "/":
-                resultLabel.text = String(firstInput / secondInput)
+                resultLabel.text = String(input[0] / input[1])
+            case "%":
+                resultLabel.text = String(input[0].truncatingRemainder(dividingBy: input[1]))
+            case "Count":
+                resultLabel.text = String(input.count)
+            case "Avg":
+                var count: Double = 0
+                for item in input{
+                    count += Double(item)
+                }
+                resultLabel.text = String(count/Double(input.count))
             default:
                 resultLabel.text = ""
             }
-            firstInput = 0.0
-            secondInput = 0.0
+            input = [Double]()
             operationInput = ""
-            operationSelected = false
-            resultLabel.text = ""
+            operationEntered = false
+        } else if operationInput == "Fact" {
+            var result = 1
+            for index in 1...Int(input[0]) {
+                result *= index
+            }
+            resultLabel.text = String(result)
         }
     }
     
     @IBAction func clearPressed(_ sender: UIButton) {
-        resultLabel.text = nil
+        resultLabel.text = ""
     }
 }
